312231c989587ebbb10707962801e17ddd5cb62f
yubbi33 <yubbi33@gmail.com>
2023-06-27 12:02:54 +0900

    Initial commit

---
+++ src/main/java/com/hamonsoft/cportal/CportalApplication.java
@@ -0,0 +1,13 @@
+package com.hamonsoft.cportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CportalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamonsoft/cportal/CportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamonsoft.cportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

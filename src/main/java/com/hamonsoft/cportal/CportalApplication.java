814938274e1fd8ddec4af9f1c4de684662fc73a9
yubbi33 <yubbi33@gmail.com>
2023-07-05 09:58:05 +0900

    war build 추가

--- src/main/java/com/hamonsoft/cportal/CportalApplication.java
+++ src/main/java/com/hamonsoft/cportal/CportalApplication.java
@@ -2,9 +2,18 @@ package com.hamonsoft.cportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.servlet.ServletInputStream;
 
 @SpringBootApplication
-public class CportalApplication {
+public class CportalApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServletInputStream.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CportalApplication.class, args);

e684ef0d0b409348979529cb6228521b502000b2
yubbi33 <yubbi33@gmail.com>
2023-07-06 13:49:57 +0900

    JPA 테스트 환경으로 전환

--- src/main/java/com/hamonsoft/cportal/config/MyBatisConfig.java
+++ src/main/java/com/hamonsoft/cportal/config/MyBatisConfig.java
@@ -14,31 +14,31 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
-@MapperScan(value = "com.hamonsoft.cportal", sqlSessionFactoryRef = "SqlSessionFactory")
-public class MyBatisConfig {
-
-    @Value("${spring.datasource.mapper-locations}")
-    String mPath;
-
-    @Bean(name = "dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource DataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-
-    @Bean(name = "SqlSessionFactory")
-    public SqlSessionFactory SqlSessionFactory(@Qualifier("dataSource") DataSource DataSource, ApplicationContext applicationContext) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(DataSource);
-        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mPath));
-        return sqlSessionFactoryBean.getObject();
-    }
-
-    @Bean(name = "SessionTemplate")
-    public SqlSessionTemplate SqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory firstSqlSessionFactory) {
-        return new SqlSessionTemplate(firstSqlSessionFactory);
-    }
-
-}
+//@Configuration
+//@MapperScan(value = "com.hamonsoft.cportal", sqlSessionFactoryRef = "SqlSessionFactory")
+//public class MyBatisConfig {
+//
+//    @Value("${spring.datasource.mapper-locations}")
+//    String mPath;
+//
+//    @Bean(name = "dataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource DataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//
+//    @Bean(name = "SqlSessionFactory")
+//    public SqlSessionFactory SqlSessionFactory(@Qualifier("dataSource") DataSource DataSource, ApplicationContext applicationContext) throws Exception {
+//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+//        sqlSessionFactoryBean.setDataSource(DataSource);
+//        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mPath));
+//        return sqlSessionFactoryBean.getObject();
+//    }
+//
+//    @Bean(name = "SessionTemplate")
+//    public SqlSessionTemplate SqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory firstSqlSessionFactory) {
+//        return new SqlSessionTemplate(firstSqlSessionFactory);
+//    }
+//
+//}

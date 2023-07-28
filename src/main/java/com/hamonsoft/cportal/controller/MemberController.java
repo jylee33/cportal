fdb8541417eb806cd98e06566eaaf85b98d31ddf
yubbi33 <yubbi33@gmail.com>
2023-07-28 15:07:47 +0900

    JPA 머지 준비

--- src/main/java/com/hamonsoft/cportal/controller/MemberController.java
+++ src/main/java/com/hamonsoft/cportal/controller/MemberController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
@@ -20,12 +22,12 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/members/new")
+    @GetMapping("new")
     public String createForm() {
-        return "members/createMemberForm";
+        return "thymeleaf/members/createMemberForm";
     }
 
-    @PostMapping("/members/new")
+    @PostMapping("new")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
@@ -35,12 +37,12 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @GetMapping("/members")
+    @GetMapping("")
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
 
-        return "members/memberList";
+        return "thymeleaf/members/memberList";
     }
 
 }

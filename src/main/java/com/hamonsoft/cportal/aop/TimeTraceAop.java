668d9b157acfa6545763330e4bf3e047f883e6ac
yubbi33 <yubbi33@gmail.com>
2023-08-04 16:57:06 +0900

    실행 시간을 측정하는 AOP 추가

---
+++ src/main/java/com/hamonsoft/cportal/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package com.hamonsoft.cportal.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    @Around("execution(* com.hamonsoft.cportal..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START : " + joinPoint.toString());
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}

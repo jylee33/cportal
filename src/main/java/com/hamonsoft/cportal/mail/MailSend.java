beee3689ef34a94c192d00f01f0e2c5a019630b1
yubbi33 <yubbi33@gmail.com>
2023-08-24 17:59:53 +0900

    비밀번호 찾기 완료

--- src/main/java/com/hamonsoft/cportal/mail/MailSend.java
+++ src/main/java/com/hamonsoft/cportal/mail/MailSend.java
@@ -252,4 +252,49 @@ public class MailSend {
 
     }
 
+    public void MailSendResetPW(String mailto, String pw) {
+        Properties prop = System.getProperties();
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        String mail_id = "cloud@hamonsoft.co.kr";
+        String mail_pw = "wtzhnnyphsiohohi";
+
+        Authenticator auth = new MailAuth(mail_id, mail_pw);
+
+        Session session = Session.getDefaultInstance(prop, auth);
+
+        MimeMessage msg = new MimeMessage(session);
+
+        try {
+            msg.setSentDate(new Date());
+
+            msg.setFrom(new InternetAddress("yubbi33@gmail.com", "HAMONSOFT"));
+//            InternetAddress to = new InternetAddress("jylee@hamonsoft.co.kr");
+            InternetAddress to = new InternetAddress(mailto);
+            msg.setRecipient(Message.RecipientType.TO, to);
+            msg.setSubject("Hamonsoft NETIS CLOUD 비밀번호 변경 안내", "UTF-8");
+            String body = "<H1>안녕하세요. Hamonsoft NETIS CLOUD 비밀번호 변경 안내 메일입니다.</H1>"
+                    + "<br>변경된 비밀번호는 <b>" + pw + "</b> 입니다."
+                    + "<br>로그인 후 비밀번호를 꼭 변경하시기 바랍니다."
+                    + "<P><img src=\"http://hamonsoft.co.kr/wp-content/uploads/2019/07/it-specialist0.png\">"
+                    + "<br><a href=\"http://hamonsoft.co.kr\">하몬소프트</a>";
+            msg.setContent(body, "text/html;charset=utf-8");
+
+            Transport.send(msg);
+
+        } catch (AddressException ae) {
+            System.out.println("AddressException : " + ae.getMessage());
+        } catch (MessagingException me) {
+            System.out.println("MessagingException : " + me.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("UnsupportedEncodingException : " + e.getMessage());
+        }
+
+    }
+
 }

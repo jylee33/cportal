2a9589b1cf1d54bb472e785ded34426e97880f62
yubbi33 <yubbi33@gmail.com>
2023-09-05 16:02:55 +0900

    jqgrid 사용 예제 추가

---
+++ src/main/java/com/hamonsoft/cportal/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.hamonsoft.cportal.controller;
+
+import com.hamonsoft.cportal.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    TestService testService;
+
+    @Autowired
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
+    @GetMapping(value = "jqgrid")
+    public void jqgrid(Model model) {
+
+    }
+
+
+}
